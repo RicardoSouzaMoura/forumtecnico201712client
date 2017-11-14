3071ce9710fe11d18dd1812086d5198392bca1c9
Joao Franco <joaofranco@iMac-de-y7lc.local>
2017-11-14 15:01:13 -0200

    toast ao enviar push

--- src/app/send-push/send-push.component.ts
+++ src/app/send-push/send-push.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PushService } from '../push.service';
-
+import {ToastyService, ToastyConfig, ToastOptions, ToastData} from 'ng2-toasty';
 
 @Component({
   selector: 'app-send-push',
@@ -10,7 +10,12 @@ import { PushService } from '../push.service';
 })
 export class SendPushComponent implements OnInit {
 
-  constructor(private pushService: PushService) { }
+  constructor(private pushService: PushService, 
+    private toastyService:ToastyService, 
+    private toastyConfig: ToastyConfig) { 
+      this.toastyConfig.theme = 'bootstrap';
+      this.toastyConfig.position = 'top-right';
+    }
   
   pushMessage : string;
 
@@ -21,9 +26,32 @@ export class SendPushComponent implements OnInit {
   onSendPushes(){
     this.pushService.sendPushes(this.pushMessage)
           .subscribe(
-            ()=>{console.log("Sucesso")},
-            (error)=>{console.log(`erro: ${error}`)}
-          );
+            () => {
+              console.log("Sucesso");
+              this.addSuccessToast();
+          },
+            (error) => {
+              console.log(`erro: ${error}`);
+              this.addErrorToast();
+          });
+  }
+  addSuccessToast() {
+    var toastOptions:ToastOptions = {
+      title: "Sucesso",
+      msg: "Push Enviado.",
+      showClose: true,
+      timeout: 5000
+    };
+    this.toastyService.success(toastOptions);
   }
 
+  addErrorToast() {
+    var toastOptions:ToastOptions = {
+      title: "Erro",
+      msg: "Algo deu errado.",
+      showClose: true,
+      timeout: 5000
+    };
+    this.toastyService.error(toastOptions);
+  }
 }
